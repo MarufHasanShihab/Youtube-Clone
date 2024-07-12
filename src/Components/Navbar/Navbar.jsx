1508f2376b5330608b337db72a49ce722918210f
Maruf Hasan Shihab <marufhasanshihab2@gmail.com>
2024-07-12 22:49:33 +0600

    added navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import './Navbar.css'
+import menu_icon from '../../assets/menu.png';
+import logo from '../../assets/logo.png';
+import search_icon from '../../assets/search.png';
+import upload_icon from '../../assets/upload.png';
+import more_icon from '../../assets/more.png';
+import notifaction_icon from '../../assets/notification.png';
+import profile_icon from '../../assets/jack.png';
+
+const Navbar = () => {
+    return (
+        <nav className='flex-div'>
+            <div className='nav-left flex-div'>
+                <img src={menu_icon} className='menu-icon' />
+                <img src={logo} className='logo' />
+            </div> 
+            <div className="nav-middle flex-div">
+                  <div className="search-box flex-div">
+                  <input type='text' placeholder='Search'/>
+                  <img src={search_icon} />
+                  </div>
+            </div>
+            <div className="nav-right flex-div">
+                <img src={upload_icon} />
+                <img src={more_icon} />
+                <img src={notifaction_icon} />
+                <img src={profile_icon} className='user-icon' />
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
